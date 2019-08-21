9eb0b26e7cf6f210dbb198b0c84637b20d180451
RKhatua <32603058+RKhatua@users.noreply.github.com>
2019-08-21 15:53:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/tasks.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 
 db = SQLAlchemy(app)
 
